2cb67aa69b0ddf7def0402b3577fc864baa3678f
Hugo Gresse <hugo.gresse@teads.tv>
2015-05-18 15:00:56 +0200

    Base Implementation of Camera Preview

--- app/src/main/java/xjet/fr/pixaline/MainActivity.java
+++ app/src/main/java/xjet/fr/pixaline/MainActivity.java
@@ -1,17 +1,73 @@
 package xjet.fr.pixaline;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    public static final String LOG_TAG = "MainActivity";
+
+    protected Camera mCamera;
+
+    SurfaceView mSurfaceView;
+    SurfaceHolder mSurfaceHolder;
+
+    Camera.PictureCallback mJpegCallback;
+
+    Camera.PictureCallback rawCallback;
+    Camera.ShutterCallback shutterCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        mSurfaceHolder = mSurfaceView.getHolder();
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mSurfaceHolder.addCallback(this);
+
+        // deprecated setting, but required on Android versions prior to 3.0
+        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        mJpegCallback = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream = null;
+                try {
+                    // TODO : use external storage
+                    outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream.write(data);
+                    outStream.close();
+                    Log.d(LOG_TAG, "onPictureTaken - wrote bytes: " + data.length);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getApplicationContext(), "Picture Saved", Toast.LENGTH_SHORT).show();
+                refreshCamera();
+            }
+        };
+
     }
 
     @Override
@@ -34,5 +90,127 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void captureImage(View v) throws IOException {
+        //take the picture
+        mCamera.takePicture(null, null, mJpegCallback);
+    }
+
+    public void refreshCamera() {
+        if (mSurfaceHolder.getSurface() == null) {
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception ignored) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        setupCamera();
+    }
+
+    private void setupCamera(){
+        // modify parameter
+        Camera.Parameters parameters = mCamera.getParameters();
+        List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+        Camera.Size cs = sizes.get(0);
+        parameters.setPreviewSize(cs.width, cs.height);
+        mCamera.setParameters(parameters);
+
+        setCameraDisplayOrientation();
+
+        try {
+            // The Surface has been created, now tell the camera where to draw
+            // the preview.
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setCameraDisplayOrientation(){
+        if (mCamera == null) {
+            Log.d(LOG_TAG, "setCameraDisplayOrientation - camera null");
+            return;
+        }
+
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(getCameraInformationId(), info);
+
+        WindowManager winManager = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        int rotation = winManager.getDefaultDisplay().getRotation();
+
+        int degrees = 0;
+
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT)
+        {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        mCamera.setDisplayOrientation(result);
+    }
+
+    private int getCameraInformationId(){
+        int cameraId = -1;
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                Log.d(LOG_TAG, "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    /*********************************************
+     * Impelment SurfaceHolder.Callback
+     *********************************************/
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        refreshCamera();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // open the camera
+            mCamera = Camera.open();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+
+        setupCamera();
+    }
+
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        mCamera.stopPreview();
+        mCamera.release();
+        mCamera = null;
     }
 }
